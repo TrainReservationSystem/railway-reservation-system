ab44558472d753b7bea969134cadfbf4c1d0d66b
D4-80592-JayeshSuthar <sutharjayesh32@gmail.com>
2023-12-25 19:09:44 +0530

    App.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import Landing from "./components/landing/Landing";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import PassengerDetails from "./components/bookings/PassengerDetails";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Landing />
-       */}
-
-       <PassengerDetails/>
+      <Landing />
     </BrowserRouter>
   );
 };
