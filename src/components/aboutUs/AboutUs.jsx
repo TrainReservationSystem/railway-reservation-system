58067f0c15d1695973db93cf306f49d45a9f4f22
D4-80593-MeetVasani <w.vasanimeet@gmail.com>
2023-12-25 14:53:07 +0530

    modified about us

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from '../common/Header1';
+import Footer from '../common/Footer';
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const AboutUs = () => {
     return (
-        <div>
-            This is about us !!
-        </div>
+        <>
+            <div>
+            <Header/>
+                This is about us !!
+            <Footer/>
+            </div>
+        </>
     );
 }
 
---
+++ src/components/train/TrainList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../common/Footer'
+import Header from '../common/Header1'
+
+const TrainList = () => {
+  return (
+    <>
+        <div>
+            <Header/>
+                <div className="container">
+                    
+                </div>
+            <Footer/>
+        </div>
+    </>
+  )
+}
+
+export default TrainList
