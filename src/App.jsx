b9d532cd2404701827d30861af192ac6045906fc
D4-80593-MeetVasani <w.vasanimeet@gmail.com>
2023-12-25 18:32:23 +0530

    modified train logo and added background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+// App.js
 import React from "react";
 import Landing from "./components/landing/Landing";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TrainList from "./components/train/TrainList";
 
 const App = () => {
+  const appStyles = {
+    backgroundColor: "#C5FFF8", 
+    minHeight: "100vh", 
+  };
+
   return (
-    <BrowserRouter>
-      <Landing />
-    </BrowserRouter>
+    <div style={appStyles}>
+      <BrowserRouter>
+        <Landing />
+        {/* <TrainList /> */}
+      </BrowserRouter>
+    </div>
   );
 };
 
