1b7b29d49b5abde86b8dcbe53709b8ee51f97500
D4-80593-MeetVasani <w.vasanimeet@gmail.com>
2023-12-27 16:33:01 +0530

    Added train status

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import TrainStatus from "../train/TrainStatus";
 
 const AboutUs = () => {
   return (
     <>
-      <div className="container text-center">
+      {/* <div className="container text-center">
         <h1>About Us !</h1>
-      </div>
+      </div> */}
+      <TrainStatus />
     </>
   );
 };
