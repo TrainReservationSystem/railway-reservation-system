5cc622bb4bbfc0817fc8db1fdddd8070f522a3a9
Meet Vasani <w.vasanimeet@gmail.com>
2024-02-06 18:54:00 +0530

    added some fields in add train

--- new/src/components/bookings/bookingsIndex.js
+++
@@ -1,7 +0,0 @@
-import BookingSuccess from "./BookingSuccess"
-import PassengerDetails from "./PassengerDetails"
-import PaymentPage from './PaymentPage'
-import BookingCard from "./BookingCard"
-
-export {BookingSuccess,PassengerDetails,PaymentPage,BookingCard}
-
--- new/src/components/train/trainIndex.js
+++
@@ -1,7 +0,0 @@
-import AddTrain from "./AddTrain";
-import TrainList from "./TrainList";
-import TrainStatus from "./TrainStatus";
-import TrainManagement from "./TrainManagement";
-
-export {AddTrain,TrainList,TrainStatus,TrainManagement}
-
